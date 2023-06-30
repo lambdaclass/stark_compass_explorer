288103434c7a675069fa01b9a39c292e734f774f
Francisco Krause Arnim <fkrausear@gmail.com>
2023-06-30 13:20:44 -0300

    Fix rpc module, comment Ecto process

--- lib/starknet_rpc.ex
+++ lib/starknet_rpc.ex
@@ -2,7 +2,8 @@ defmodule StarknetExplorer.Rpc do
   use Tesla
 
   plug Tesla.Middleware.BaseUrl,
-       "https://starknet-mainnet.infura.io/v3/" <> Application.compile_env(:rpc, :api_key)
+       "https://starknet-mainnet.infura.io/v3/" <>
+         Application.fetch_env!(:starknet_explorer, :api_key)
 
   plug Tesla.Middleware.JSON
 end
