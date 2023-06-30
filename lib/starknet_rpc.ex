b55b9779c67cf132d8b6677cdd7fb5ba0229b53c
Francisco Krause Arnim <fkrausear@gmail.com>
2023-06-30 13:18:43 -0300

    Fix rpc module

--- lib/starknet_rpc.ex
+++ lib/starknet_rpc.ex
@@ -1,4 +1,8 @@
 defmodule StarknetExplorer.Rpc do
-  plug Tesla.Middleware.BaseUrl, "https://starknet-mainnet.infura.io/v3/" <> Application.compile_env(:rpc, :api_key)
+  use Tesla
+
+  plug Tesla.Middleware.BaseUrl,
+       "https://starknet-mainnet.infura.io/v3/" <> Application.compile_env(:rpc, :api_key)
+
   plug Tesla.Middleware.JSON
 end
