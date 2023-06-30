8bb8f163e1d58ff755732223500b68635784704b
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2023-06-30 16:11:43 -0300

    add route for transactions index

--- lib/starknet_explorer_web/router.ex
+++ lib/starknet_explorer_web/router.ex
@@ -18,6 +18,7 @@ defmodule StarknetExplorerWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    live "/transactions", TransactionsLive, :index
   end
 
   # Other scopes may use custom stacks.
