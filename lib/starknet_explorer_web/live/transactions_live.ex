8668af21f401448c54feb87f0dd286fda5457582
Nicolas Sanchez <sanchez.nicolas96@gmail.com>
2023-06-30 17:31:35 -0300

    rename functions and refactor live functions

--- lib/starknet_explorer_web/live/transactions_live.ex
+++ lib/starknet_explorer_web/live/transactions_live.ex
@@ -1,6 +1,7 @@
 defmodule StarknetExplorerWeb.TransactionsLive do
   use StarknetExplorerWeb, :live_view
   alias StarknetExplorer.Rpc
+  alias StarknetExplorer.DateUtils
 
   def render(assigns) do
     ~H"""
@@ -11,7 +12,6 @@ defmodule StarknetExplorerWeb.TransactionsLive do
           <th>Type</th>
           <th>Status</th>
           <th>Age</th>
-          <th></th>
         </tr>
       </thead>
       <tbody id="transactions">
@@ -21,7 +21,7 @@ defmodule StarknetExplorerWeb.TransactionsLive do
               <td><%= transaction["transaction_hash"] %></td>
               <td><%= transaction["type"] %></td>
               <td><%= block["status"] %></td>
-              <td><%= block["age"] %></td>
+              <td><%= get_block_age(block) %></td>
             </tr>
           <% end %>
         <% end %>
@@ -31,31 +31,27 @@ defmodule StarknetExplorerWeb.TransactionsLive do
   end
 
   def mount(_params, _session, socket) do
-    blocks_with_transactions =
-      get_transactions_from_last_ten_blocks()
-      |> Enum.map(fn block -> Map.put(block, "age", get_block_age(block)) end)
-
-    {:ok, assign(socket, [blocks_with_transactions: blocks_with_transactions])}
+    {:ok, assign(socket, [blocks_with_transactions: get_last_ten_blocks_with_transactions()])}
   end
 
-  defp get_transactions_from_last_ten_blocks() do
-    for block_number <- get_last_ten_block_numbers() do
-      {:ok, block} = StarknetExplorer.Rpc.get_block_by_number(block_number)
+  defp get_last_ten_blocks_with_transactions() do
+    get_last_ten_block_numbers()
+    |> Enum.map(fn block_number ->
+      {:ok, block} = Rpc.get_block_by_number(block_number)
       block
-    end
+    end)
   end
 
   defp get_last_ten_block_numbers() do
-    {:ok, %{"block_number" => last_block_number}} = StarknetExplorer.Rpc.get_last_block()
+    {:ok, %{"block_number" => last_block_number}} = Rpc.get_latest_block()
 
     # I take a range of 10 blocks
     (last_block_number - 10)..last_block_number
     |> Enum.to_list()
   end
 
-
   defp get_block_age(block) do
-    %{minutes: minutes, hours: hours, days: days} = StarknetExplorer.DateUtils.calculate_time_difference(block["timestamp"])
+    %{minutes: minutes, hours: hours, days: days} = DateUtils.calculate_time_difference(block["timestamp"])
     case days do
       0 -> case hours do
         0 -> "#{minutes} min"
