9fc047ad2bb6bdf11a073b47aeed6d5abc27b76b
Francisco Krause Arnim <fkrausear@gmail.com>
2023-07-03 19:03:37 -0300

    Add search bar to layout

---
+++ lib/starknet_explorer_web/live/search.ex
@@ -0,0 +1,83 @@
+defmodule StarknetExplorerWeb.SearchLive do
+  use StarknetExplorerWeb, :live_view
+  alias StarknetExplorer.Rpc
+
+  def render(assigns) do
+    ~H"""
+    <form phx-change="update-input" phx-submit="search">
+      <input
+        type="text"
+        name="search-input"
+        value={@query}
+        list="matches"
+        placeholder="Search..."
+        {%{readonly: @loading}}
+      />
+      <%= for error <- @errors do %>
+        <pre><%= error %></pre>
+      <% end %>
+    </form>
+    """
+  end
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, query: "", loading: false, matches: [], errors: [])}
+  end
+
+  def handle_event("update-input", %{"search-input" => query} , socket) do
+    {:noreply, assign(socket, :query, query)}
+  end
+
+  def handle_event("search", %{"search-input" => query}, socket) when byte_size(query) <= 100 do
+    send(self(), {:search, query})
+    {:noreply, assign(socket, query: query, result: "Searching...", loading: true, matches: [])}
+  end
+
+  def handle_info({:search, query}, socket) do
+    query = String.trim(query)
+    navigate_fun =
+      case try_search(query) do
+        {:tx, _tx} ->
+          fn -> push_navigate(socket, to: ~p"/transactions/#{query}") end
+        {:block, _block} ->
+          fn -> push_navigate(socket, to: ~p"/block/#{query}") end
+        :noquery -> fn -> assign(socket, errors: ["Couldn't find something matching your query"]) end
+      end
+    {:noreply, navigate_fun.()}
+  end
+
+  defp try_search(query) do
+    case infer_query(query) do
+      :hex -> try_by_hash(query)
+      {:number, number} -> try_by_number(number)
+      :noquery -> :noquery
+    end
+  end
+
+  def try_by_number(number) do
+    case Rpc.get_block_by_number(number) do
+      {:ok, block} -> {:block, number}
+      {:error, :not_found} -> :noquery
+    end
+  end
+
+  def try_by_hash(hash) do
+    case Rpc.get_transaction(hash) do
+      {:ok, transaction} -> {:tx, hash}
+      {:error, _} ->
+        case Rpc.get_block_by_hash(hash) do
+          {:ok, block} -> {:block, block}
+          {:error, _} -> :noquery
+        end
+    end
+  end
+
+  defp infer_query(query = <<"0x", rest::binary>>), do: :hex
+  defp infer_query(query) do
+    case Integer.parse(query) do
+      {parsed, ""} -> {:number, parsed}
+      _ -> :noquery
+    end
+  end
+  defp infer_query(query), do: :noquery
+end
