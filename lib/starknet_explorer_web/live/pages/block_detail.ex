1184b87696ab92129ea1c9b752ac2fc1e2840761
Francisco Krause Arnim <fkrausear@gmail.com>
2023-07-04 16:32:30 -0300

    Finish block detail hardcoding

--- lib/starknet_explorer_web/live/pages/block_detail.ex
+++ lib/starknet_explorer_web/live/pages/block_detail.ex
@@ -1,7 +1,7 @@
 defmodule StarknetExplorerWeb.BlockDetailLive do
   use StarknetExplorerWeb, :live_view
   alias StarknetExplorer.Rpc
-
+  alias StarknetExplorerWeb.Utils
   defp num_or_hash(<<"0x", _rest::binary>>), do: :hash
   defp num_or_hash(_num), do: :num
 
@@ -19,6 +19,10 @@ defmodule StarknetExplorerWeb.BlockDetailLive do
     {:ok, assign(socket, :block, block)}
   end
 
+  # TODO:
+  # Do not hardcode:
+  # - Total Execeution Resources
+  # - Gas Price
   def render(assigns = %{block: _block}) do
     ~H"""
     <div class="flex justify-center items-center pt-14">
@@ -28,10 +32,13 @@ defmodule StarknetExplorerWeb.BlockDetailLive do
       <thead>
         <ul>
           <li>Block Number <%= @block["block_number"] %></li>
-          <li>New Root <%= @block["new_root"] %></li>
-          <li>Parent Hash <%= @block["parent_hash"] %></li>
+          <li>Block Hash <%= @block["block_hash"] |> Utils.shorten_block_hash %></li>
+          <li>Block Status <%= @block["status"] %></li>
+          <li>State Root <%= @block["new_root"] |> Utils.shorten_block_hash  %></li>
+          <li>Parent Hash <%= @block["parent_hash"] |> Utils.shorten_block_hash  %></li>
           <li>Sequencer Address <%= @block["sequencer_address"] %></li>
-          <li>Status <%= @block["status"] %></li>
+          <li>Gas Price <%= "0.000000017333948464 ETH" %></li>
+          <li>Total execution resources <%= 543910 %></li>
           <li>
             Timestamp <%= @block["timestamp"]
             |> DateTime.from_unix()
@@ -42,7 +49,22 @@ defmodule StarknetExplorerWeb.BlockDetailLive do
       <tbody id="transactions">
         <h1>Block Transactions</h1>
         <%= for _transaction = %{"transaction_hash" => hash, "type" => type, "version" => version} <- @block["transactions"] do %>
-          Type: <%= hash %> Hash: <%= type %> Version: <%= version %>
+        <table>
+        <thead>
+        <tr>
+        <th> Type </th>
+        <th> Version </th>
+        <th> Hash </th>
+        </tr>
+        <tbody>
+        <tr>
+        <td> <%= type %> </td>
+        <td> <%= version %> </td>
+        <td> <%= hash |> Utils.shorten_block_hash %> </td>
+        </tr>
+        </tbody>
+        </thead>
+        </table>
         <% end %>
       </tbody>
     </table>
