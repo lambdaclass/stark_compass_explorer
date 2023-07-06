acc219ad9304dc2cbd6283bb20d075c69f08844a
Francisco Krause Arnim <fkrausear@gmail.com>
2023-07-06 12:20:37 -0300

    Format

--- test/block_fetcher_test.exs
+++ test/block_fetcher_test.exs
@@ -2,14 +2,16 @@ defmodule StarknetExplorer.BlockFetcher.Test do
   alias StarknetExplorer.{BlockFetcher, Block, Rpc, Repo}
   use StarknetExplorer.DataCase
 
-  test "Fetch some blocks during 10 seconds, check db is ok" do
+  test "Fetch some blocks during 10 seconds, check db" do
     {:ok, pid} = BlockFetcher.start_link([])
     Process.send_after(pid, :stop, 10_000)
     Process.sleep(10_000)
     block_height = Block.highest_fetched_block_number()
 
     for i <- 1..block_height do
-      {:ok, %{"block_hash" => hash, "block_number" => number}} = Rpc.get_block_by_number(i)
+      {:ok, %{"block_hash" => hash, "block_number" => number, "transactions" => tx}} =
+        Rpc.get_block_by_number(i)
+
       db_block = Repo.one!(from b in Block, where: b.number == ^i)
       assert hash == db_block.hash
       assert number == db_block.number
