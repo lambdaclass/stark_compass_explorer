f78fb628b604b4d6facd25293f34e43d673a4b3b
Francisco Krause Arnim <fkrausear@gmail.com>
2023-07-12 18:50:51 -0300

    Format

--- lib/starknet_explorer_web/live/pages/home/index.ex
+++ lib/starknet_explorer_web/live/pages/home/index.ex
@@ -38,7 +38,7 @@ defmodule StarknetExplorerWeb.HomeLive.Index do
         <div class="text-sm">
           <div>TPS</div>
           <div>
-          <%= live_render(@socket, TPSComponent, id: "tps-number") %>
+            <%= live_render(@socket, TPSComponent, id: "tps-number") %>
           </div>
         </div>
       </div>
