088eae8fe6dce54fc78c11f2d25e843a4d7abda9
An <82987608+AngieDutra@users.noreply.github.com>
2023-07-13 18:16:27 -0300

    events detail responsive

--- lib/starknet_explorer_web/live/event_live.ex
+++ lib/starknet_explorer_web/live/event_live.ex
@@ -9,45 +9,85 @@ defmodule StarknetExplorerWeb.EventDetailLive do
       id: "search-bar",
       flash: @flash
     ) %>
-    <div class="flex justify-center items-center pt-14">
-      <h1 class="text-white text-4xl font-mono">Event Detail</h1>
-    </div>
-    <table>
-      <thead>
-        <ul>
-          <li>
-            Event ID 0x01b4d24a461851e8eb9924369b5d9e23e79e8bbea6abc93eae4323462a25ddac_1
-          </li>
-          <li>Block Hash 0x014570bdb1ed38e71bb11709b2fff208101d74231c1b973bd5d1e3ab717c659a</li>
-          <li>Block Number 98369</li>
-          <li>Transaction Hash 0x01b4d24a461851e8eb9924369b5d9e23e79e8bbea6abc93eae4323462a25ddac</li>
-          <li>Contract Address 0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7</li>
-          <li>L1 Block Hash 0x93f4c763b869dd008594f8a976c7f0bb60942db7bf314a70339e7e8c94fcfd1a</li>
-          <li>L1 Block Number 17630003</li>
-          Event Data
-          <div class="table-block">
-            <ul class="transactions-grid table-th">
-              <li class="col-span-2">Input</li>
-              <li class="col-span-2">Type</li>
-              <li class="col-span-2">Value</li>
-            </ul>
-            <div id="classes">
-              <%= for idx <- 0..0 do %>
-                <ul id={"message-#{idx}"} class="transactions-grid border-t border-gray-600">
-                  <li class="col-span-2">
-                    from_
-                  </li>
-                  <li class="col-span-2">felt</li>
-                  <li class="col-span-2">
-                    "0x4c97c4d367b88df2f29887043750ff189f539c28fe1fed331d3359b403a8bad">
-                  </li>
-                </ul>
-              <% end %>
-            </div>
+    <div class="max-w-7xl mx-auto bg-container p-4 md:p-6 rounded-md">
+      <div class="flex flex-col lg:flex-row lg:gap-2 items-baseline pb-5">
+        <h2>Event</h2>
+        <div class="font-semibold">
+          <%= "0x01b4d24a461851e8eb9924369b5d9e23e79e8bbea6abc93eae4323462a25ddac_1"
+          |> Utils.shorten_block_hash() %>
+        </div>
+      </div>
+      <div class="grid-4 custom-list-item">
+        <div class="block-label !mt-0">Event ID</div>
+        <div>
+          <%= "0x01b4d24a461851e8eb9924369b5d9e23e79e8bbea6abc93eae4323462a25ddac_1"
+          |> Utils.shorten_block_hash() %>
+        </div>
+      </div>
+      <div class="grid-4 custom-list-item">
+        <div class="block-label !mt-0">Block Hash</div>
+        <div>
+          <%= "0x014570bdb1ed38e71bb11709b2fff208101d74231c1b973bd5d1e3ab717c659a"
+          |> Utils.shorten_block_hash() %>
+        </div>
+      </div>
+      <div class="grid-4 custom-list-item">
+        <div class="block-label !mt-0">Block Number</div>
+        <div>98369</div>
+      </div>
+      <div class="grid-4 custom-list-item">
+        <div class="block-label !mt-0">Transaction Hash</div>
+        <div>
+          <%= "0x01b4d24a461851e8eb9924369b5d9e23e79e8bbea6abc93eae4323462a25ddac"
+          |> Utils.shorten_block_hash() %>
+        </div>
+      </div>
+      <div class="grid-4 custom-list-item">
+        <div class="block-label !mt-0">Contract Address</div>
+        <div>
+          <%= "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
+          |> Utils.shorten_block_hash() %>
+        </div>
+      </div>
+      <div class="grid-4 custom-list-item">
+        <div class="block-label !mt-0">L1 Block Hash</div>
+        <div>
+          <%= "0x93f4c763b869dd008594f8a976c7f0bb60942db7bf314a70339e7e8c94fcfd1a"
+          |> Utils.shorten_block_hash() %>
+        </div>
+      </div>
+      <div class="grid-4 custom-list-item">
+        <div class="block-label !mt-0">L1 Block Number</div>
+        <div>17630003</div>
+      </div>
+      <div class="custom-list-item">
+        <div class="block-label !mt-0 lg:pb-5">Event Data</div>
+        <div class="bg-black/10 p-5">
+          <div class="grid-3 w-full table-th">
+            <div>Input</div>
+            <div>Type</div>
+            <div>Value</div>
           </div>
-        </ul>
-      </thead>
-    </table>
+          <%= for idx <- 0..0 do %>
+            <div class="grid-3 w-full custom-list-item">
+              <div>
+                <div class="list-h">Input</div>
+                <div>from_</div>
+              </div>
+              <div>
+                <div class="list-h">Type</div>
+                <div>felt</div>
+              </div>
+              <div>
+                <div class="list-h">Value</div>
+                <%= "0x4c97c4d367b88df2f29887043750ff189f539c28fe1fed331d3359b403a8bad"
+                |> Utils.shorten_block_hash() %>
+              </div>
+            </div>
+          <% end %>
+        </div>
+      </div>
+    </div>
     """
   end
 
