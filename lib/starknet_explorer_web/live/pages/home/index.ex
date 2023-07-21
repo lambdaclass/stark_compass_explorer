80f9ad2e9c9abf831d857d17171099ba581a7a33
Javier Chatruc <jrchatruc@gmail.com>
2023-07-21 15:25:18 +0200

    FIx empty latest transactions page

--- lib/starknet_explorer_web/live/pages/home/index.ex
+++ lib/starknet_explorer_web/live/pages/home/index.ex
@@ -10,7 +10,8 @@ defmodule StarknetExplorerWeb.HomeLive.Index do
      assign(socket,
        blocks: [],
        latest_block: [],
-       block_height: "Loading..."
+       block_height: "Loading...",
+       transactions: []
      )}
   end
 
@@ -177,55 +178,53 @@ defmodule StarknetExplorerWeb.HomeLive.Index do
             <div class="col-span-2">Status</div>
             <div>Age</div>
           </div>
-          <%= for block <- @latest_block do %>
-            <%= for {transaction, idx} <- Enum.take(Enum.with_index(block["transactions"]), 15) do %>
-              <div id={"transaction-#{idx}"} class="grid-7 custom-list-item">
-                <div class="col-span-2">
-                  <div class="list-h">Transaction Hash</div>
-                  <div class="copy-container" id={"copy-transaction-#{idx}"} phx-hook="Copy">
-                    <div class="relative">
-                      <%= live_redirect(Utils.shorten_block_hash(transaction["transaction_hash"]),
-                        to: "/transactions/#{transaction["transaction_hash"]}",
-                        class: "text-hover-blue"
-                      ) %>
-                      <div class="absolute top-1/2 -right-6 tranform -translate-y-1/2">
-                        <div class="relative">
-                          <img
-                            class="copy-btn copy-text w-4 h-4"
-                            src={~p"/images/copy.svg"}
-                            data-text={transaction["transaction_hash"]}
-                          />
-                          <img
-                            class="copy-check absolute top-0 left-0 w-4 h-4 opacity-0 pointer-events-none"
-                            src={~p"/images/check-square.svg"}
-                          />
-                        </div>
+          <%= for {transaction, idx} <- Enum.take(Enum.with_index(@transactions), 15) do %>
+            <div id={"transaction-#{idx}"} class="grid-7 custom-list-item">
+              <div class="col-span-2">
+                <div class="list-h">Transaction Hash</div>
+                <div class="copy-container" id={"copy-transaction-#{idx}"} phx-hook="Copy">
+                  <div class="relative">
+                    <%= live_redirect(Utils.shorten_block_hash(transaction["transaction_hash"]),
+                      to: "/transactions/#{transaction["transaction_hash"]}",
+                      class: "text-hover-blue"
+                    ) %>
+                    <div class="absolute top-1/2 -right-6 tranform -translate-y-1/2">
+                      <div class="relative">
+                        <img
+                          class="copy-btn copy-text w-4 h-4"
+                          src={~p"/images/copy.svg"}
+                          data-text={transaction["transaction_hash"]}
+                        />
+                        <img
+                          class="copy-check absolute top-0 left-0 w-4 h-4 opacity-0 pointer-events-none"
+                          src={~p"/images/check-square.svg"}
+                        />
                       </div>
                     </div>
                   </div>
                 </div>
-                <div class="col-span-2">
-                  <div class="list-h">Type</div>
-                  <div>
-                    <span class={"#{if transaction["type"] == "INVOKE", do: "violet-label", else: "lilac-label"}"}>
-                      <%= transaction["type"] %>
-                    </span>
-                  </div>
-                </div>
-                <div class="col-span-2">
-                  <div class="list-h">Status</div>
-                  <div>
-                    <span class={"#{if block["status"] == "ACCEPTED_ON_L2", do: "green-label"} #{if block["status"] == "ACCEPTED_ON_L1", do: "blue-label"} #{if block["status"] == "PENDING", do: "pink-label"}"}>
-                      <%= block["status"] %>
-                    </span>
-                  </div>
-                </div>
+              </div>
+              <div class="col-span-2">
+                <div class="list-h">Type</div>
                 <div>
-                  <div class="list-h">Age</div>
-                  <%= Utils.get_block_age(block) %>
+                  <span class={"#{if transaction["type"] == "INVOKE", do: "violet-label", else: "lilac-label"}"}>
+                    <%= transaction["type"] %>
+                  </span>
                 </div>
               </div>
-            <% end %>
+              <div class="col-span-2">
+                <div class="list-h">Status</div>
+                <div>
+                  <span class={"#{if transaction["block_status"] == "ACCEPTED_ON_L2", do: "green-label"} #{if transaction["block_status"] == "ACCEPTED_ON_L1", do: "blue-label"} #{if transaction["block_status"] == "PENDING", do: "pink-label"}"}>
+                    <%= transaction["block_status"] %>
+                  </span>
+                </div>
+              </div>
+              <div>
+                <div class="list-h">Age</div>
+                <%= Utils.get_block_age_from_timestamp(transaction["block_timestamp"]) %>
+              </div>
+            </div>
           <% end %>
         </div>
       </div>
@@ -236,10 +235,21 @@ defmodule StarknetExplorerWeb.HomeLive.Index do
   @impl true
   def handle_info(:load_blocks, socket) do
     latest_block = Utils.get_latest_block_with_transactions()
+    blocks = Utils.list_blocks()
+
+    transactions =
+      Enum.map(blocks, fn block ->
+        Enum.map(block["transactions"], fn transaction ->
+          Map.put(transaction, "block_status", block["status"])
+          |> Map.put("block_timestamp", block["timestamp"])
+        end)
+      end)
+      |> List.flatten()
 
     {:noreply,
      assign(socket,
-       blocks: Utils.list_blocks(),
+       blocks: blocks,
+       transactions: transactions,
        latest_block: latest_block,
        block_height: latest_block |> hd |> Map.get("block_number")
      )}
