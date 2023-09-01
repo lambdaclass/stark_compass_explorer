6da1ebf7bbc8c9eae23cc0d0af4286d36640f3d1
Francisco Krause Arnim <fkrausear@gmail.com>
2023-09-01 11:35:09 -0300

    Formatting

--- priv/repo/migrations/20230829212045_add_block_fee_and_resources.exs
+++ priv/repo/migrations/20230829212045_add_block_fee_and_resources.exs
@@ -2,7 +2,7 @@ defmodule StarknetExplorer.Repo.Migrations.AddBlockFeeAndResources do
   use Ecto.Migration
 
   def change do
-    alter table :blocks do
+    alter table(:blocks) do
       add :gas_fee_in_wei, :string, null: true
       add :execution_resources, :integer, null: true
     end
