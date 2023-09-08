77ee7b23a3662bd079d2841a9adedb09836f8421
Santiago Pittella <87827390+SantiagoPittella@users.noreply.github.com>
2023-09-08 17:53:47 -0300

    modify ports (#126)

--- config/runtime.exs
+++ config/runtime.exs
@@ -90,7 +90,7 @@ if config_env() == :prod do
       """
 
   host = System.get_env("PHX_HOST") || "madarastark.com"
-  port = String.to_integer(System.get_env("PORT") || "5000")
+  port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :starknet_explorer, StarknetExplorerWeb.Endpoint,
     server: true,
