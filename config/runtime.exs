881a78efd4f497131e167758f835831c920c93b5
SantiagoPittella <pittellasantiago@gmail.com>
2023-09-14 15:52:44 -0300

    fix missing coma

--- config/runtime.exs
+++ config/runtime.exs
@@ -75,7 +75,7 @@ if config_env() == :prod do
   config :starknet_explorer, StarknetExplorer.Repo,
     # ssl: true,
     database: database_path,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     stacktrace: true,
     show_sensitive_data_on_connection_error: true
 
