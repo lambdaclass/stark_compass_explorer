ecffda5d757311b5ae6be51997e8a4496cfe98f0
Santiago Pittella <87827390+SantiagoPittella@users.noreply.github.com>
2023-09-22 15:01:34 -0300

    Add PostgreSQL (#189)
    * Add PostgreSQL
    
    * format
    
    * add dep reset in readme
    
    * Swap to postgresql in testing and production deployments
    
    * format
    
    * Add DATABASE_URL to build time env
    
    * Add missing param
    
    ---------
    
    Co-authored-by: Klaus Lungwitz <18153834+klaus993@users.noreply.github.com>

--- lib/starknet_explorer/repo.ex
+++ lib/starknet_explorer/repo.ex
@@ -1,7 +1,14 @@
 defmodule StarknetExplorer.Repo do
+  adapter =
+    if System.get_env("DB_TYPE") == "postgresql" do
+      Ecto.Adapters.Postgres
+    else
+      Ecto.Adapters.SQLite3
+    end
+
   use Ecto.Repo,
     otp_app: :starknet_explorer,
-    adapter: Ecto.Adapters.SQLite3
+    adapter: adapter
 
   use Scrivener, page_size: 30
 end
