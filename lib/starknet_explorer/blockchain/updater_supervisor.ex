94c09270dae1a778d48062e4a5ac6faff628fb4f
SantiagoPittella <pittellasantiago@gmail.com>
2023-09-22 18:58:19 -0300

    add blockchain updater

--- lib/starknet_explorer/blockchain/updater_supervisor.ex
+++ lib/starknet_explorer/blockchain/updater_supervisor.ex
@@ -8,9 +8,26 @@ defmodule StarknetExplorer.Blockchain.UpdaterSupervisor do
 
   @impl true
   def init(_init_arg) do
-    children = [
-      UpdaterWorker
-    ]
+    children =
+      if Mix.env() == :prod do
+        [
+          Supervisor.child_spec({UpdaterWorker, [network: :mainnet, name: :updater_mainnet]},
+            id: "updater_mainnet"
+          ),
+          Supervisor.child_spec({UpdaterWorker, [network: :testnet, name: :updater_testnet]},
+            id: "updater_testnet"
+          ),
+          Supervisor.child_spec({UpdaterWorker, [network: :testnet2, name: :updater_testnet2]},
+            id: "updater_testnet2"
+          )
+        ]
+      else
+        [
+          Supervisor.child_spec({UpdaterWorker, [network: :mainnet, name: :updater_mainnet]},
+            id: "updater_mainnet"
+          )
+        ]
+      end
 
     Supervisor.init(children, strategy: :one_for_one)
   end
