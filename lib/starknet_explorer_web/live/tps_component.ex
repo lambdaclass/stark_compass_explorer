14405100bbb56e704d65d4bb587aafc5514fbf52
Javier Chatruc <jrchatruc@gmail.com>
2023-09-25 16:33:46 -0300

    Fix tests

--- lib/starknet_explorer_web/live/tps_component.ex
+++ lib/starknet_explorer_web/live/tps_component.ex
@@ -1,6 +1,5 @@
 defmodule StarknetExplorerWeb.Component.TransactionsPerSecond do
   use StarknetExplorerWeb, :live_view
-  alias StarknetExplorer.Rpc
   alias StarknetExplorer.Block
 
   @impl true
@@ -14,18 +13,24 @@ defmodule StarknetExplorerWeb.Component.TransactionsPerSecond do
   def mount(_params, _session = %{"network" => network}, socket) do
     socket =
       socket
-      |> assign(tx_per_second: calculate_tps(socket, network))
+      |> assign(tx_per_second: calculate_tps(network))
 
     {:ok, socket}
   end
 
-  defp calculate_tps(socket, network) do
-    [current_block, previous_block] = Block.latest_n_blocks_with_txs(2, network)
-    curr_block_time = current_block.timestamp - previous_block.timestamp
+  defp calculate_tps(network) do
+    result = Block.latest_blocks_with_txs(2, network)
 
-    tx_amount = Enum.count(current_block.transactions)
+    if length(result) < 2 do
+      0
+    else
+      [current_block, previous_block] = result
+      curr_block_time = current_block.timestamp - previous_block.timestamp
 
-    safe_div(tx_amount, curr_block_time) |> :erlang.float_to_binary(decimals: 2)
+      tx_amount = Enum.count(current_block.transactions)
+
+      safe_div(tx_amount, curr_block_time) |> :erlang.float_to_binary(decimals: 2)
+    end
   end
 
   defp safe_div(x, y) when is_number(x) and is_number(y) and y > 0, do: x / y
