56cc6bec8225732b3181b16c1217638345835152
Gonzalo Torres <gonnza@gmail.com>
2023-09-26 17:44:27 -0300

    Fixed liveview warning

--- lib/starknet_explorer_web/components/core_components.ex
+++ lib/starknet_explorer_web/components/core_components.ex
@@ -680,15 +680,13 @@ defmodule StarknetExplorerWeb.CoreComponents do
   end
 
   def tooltip(assigns) do
-    css_class = assigns[:class] || ""
-
     ~H"""
     <img
       id={@id}
       phx-hook="Tooltip"
       data-tip={@text}
       src="/images/help-circle.svg"
-      class={"tooltip w-4 h-4 #{css_class}"}
+      class={"tooltip w-4 h-4 #{assigns[:class] || ""}"}
     />
     """
   end
