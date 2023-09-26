9762e1b69445d59fa084dd6907d13aa517f086a9
klaus993 <18153834+klaus993@users.noreply.github.com>
2023-09-26 12:37:21 -0300

    Add sentry  (#208)
    * Make workflows queue up when a job is already running
    
    https://github.com/orgs/community/discussions/55027
    
    * Add sentry to elixir
    
    https://docs.sentry.io/platforms/elixir/
    
    * Fix CI by removing the only prod requirement for sentry
    
    * Fix syntax error
    
    ---------
    
    Co-authored-by: Javier Chatruc <jrchatruc@gmail.com>

--- config/prod.exs
+++ config/prod.exs
@@ -16,3 +16,12 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
+config :sentry,
+  dsn: System.fetch_env("SENTRY_DSN"),
+  environment_name: :prod,
+  enable_source_code_context: true,
+  root_source_code_path: File.cwd!(),
+  tags: %{
+    env: System.fetch_env("SENTRY_ENV")
+  },
+  included_environments: [:prod]
