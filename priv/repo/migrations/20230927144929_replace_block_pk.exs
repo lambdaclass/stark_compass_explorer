c8ec8b16641971feea1c6d677e21549d5f7f4b43
SantiagoPittella <pittellasantiago@gmail.com>
2023-09-27 12:17:57 -0300

    replace blocks PK

---
+++ priv/repo/migrations/20230927144929_replace_block_pk.exs
@@ -0,0 +1,15 @@
+defmodule StarknetExplorer.Repo.Migrations.ReplaceBlockPk do
+  use Ecto.Migration
+
+  def change do
+    # Remove the existing primary key constraint
+    execute("ALTER TABLE blocks DROP CONSTRAINT blocks_pkey CASCADE")
+
+    # Set the "hash" column as the new primary key
+    alter table(:blocks) do
+      modify :number, :integer, primary_key: false
+    end
+    # Add a new composite primary key using a tuple with two columns
+    execute("ALTER TABLE blocks ADD PRIMARY KEY (number, network)")
+  end
+end
