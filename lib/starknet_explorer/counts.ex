ebedfd9ad96e8a71e75b7f9522242a7aef018fe2
Javier Rodríguez Chatruc <49622509+jrchatruc@users.noreply.github.com>
2023-09-27 19:25:41 -0300

    Hardcode numbers until we can set them through a diff (#284)

--- lib/starknet_explorer/counts.ex
+++ lib/starknet_explorer/counts.ex
@@ -1,9 +1,9 @@
 defmodule StarknetExplorer.Counts do
   use Ecto.Schema
   import Ecto.Query
-  # alias StarknetExplorer.Events
-  # alias StarknetExplorer.Message
-  # alias StarknetExplorer.Transaction
+  alias StarknetExplorer.Events
+  alias StarknetExplorer.Message
+  alias StarknetExplorer.Transaction
   alias StarknetExplorer.{Counts, BlockUtils}
   alias StarknetExplorer.Repo
 
@@ -27,12 +27,9 @@ defmodule StarknetExplorer.Counts do
 
   def insert_or_update(network) do
     {:ok, blocks} = BlockUtils.block_height(network)
-    # transactions = Transaction.get_total_count(network)
-    transactions = 0
-    # messages = Message.get_total_count(network)
-    messages = 0
-    # events = Events.get_total_count(network)
-    events = 0
+    transactions = Transaction.get_total_count(network)
+    messages = Message.get_total_count(network)
+    events = Events.get_total_count(network)
 
     case Repo.get_by(Counts, network: Atom.to_string(network)) do
       # Count exists, let's use it
