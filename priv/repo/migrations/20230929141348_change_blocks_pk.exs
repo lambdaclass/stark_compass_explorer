c0aa1fd6b57436db3799e0ff258eee096133c231
Javier Chatruc <jrchatruc@gmail.com>
2023-09-29 11:54:02 -0300

    Run only on postgres for now

--- priv/repo/migrations/20230929141348_change_blocks_pk.exs
+++ priv/repo/migrations/20230929141348_change_blocks_pk.exs
@@ -2,16 +2,18 @@ defmodule StarknetExplorer.Repo.Migrations.ChangeBlocksPk do
   use Ecto.Migration
 
   def change do
-    execute("alter table transactions drop constraint block_number;")
-    execute("alter table blocks drop constraint blocks_pkey;")
-    execute("alter table blocks add primary key (number, network);")
+    if System.get_env("DB_TYPE") == "postgresql" do
+      execute("alter table transactions drop constraint block_number;")
+      execute("alter table blocks drop constraint blocks_pkey;")
+      execute("alter table blocks add primary key (number, network);")
 
-    execute(
-      "alter table transactions add constraint transactions_blocks_fk foreign key (block_number, network) references blocks (number, network);"
-    )
+      execute(
+        "alter table transactions add constraint transactions_blocks_fk foreign key (block_number, network) references blocks (number, network);"
+      )
 
-    execute(
-      "alter table events add constraint events_blocks_fk foreign key (block_number, network) references blocks (number, network);"
-    )
+      execute(
+        "alter table events add constraint events_blocks_fk foreign key (block_number, network) references blocks (number, network);"
+      )
+    end
   end
 end
