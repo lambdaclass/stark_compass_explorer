62869f0624d4f280ec99aad854b578e0800ff722
Javier Chatruc <jrchatruc@gmail.com>
2023-09-29 11:45:31 -0300

    Mix format

--- priv/repo/migrations/20230929141348_change_blocks_pk.exs
+++ priv/repo/migrations/20230929141348_change_blocks_pk.exs
@@ -5,7 +5,13 @@ defmodule StarknetExplorer.Repo.Migrations.ChangeBlocksPk do
     execute("alter table transactions drop constraint block_number;")
     execute("alter table blocks drop constraint blocks_pkey;")
     execute("alter table blocks add primary key (number, network);")
-    execute("alter table transactions add constraint transactions_blocks_fk foreign key (block_number, network) references blocks (number, network);")
-    execute("alter table events add constraint events_blocks_fk foreign key (block_number, network) references blocks (number, network);")
+
+    execute(
+      "alter table transactions add constraint transactions_blocks_fk foreign key (block_number, network) references blocks (number, network);"
+    )
+
+    execute(
+      "alter table events add constraint events_blocks_fk foreign key (block_number, network) references blocks (number, network);"
+    )
   end
 end
