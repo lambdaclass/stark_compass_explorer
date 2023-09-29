d08059f74b0994c7e92d071eb4f1c5f2610306e3
Nacho <amigoigna@gmail.com>
2023-09-29 11:41:21 -0300

    alter tables

--- priv/repo/migrations/20230929141348_change_blocks_pk.exs
+++ priv/repo/migrations/20230929141348_change_blocks_pk.exs
@@ -2,19 +2,10 @@ defmodule StarknetExplorer.Repo.Migrations.ChangeBlocksPk do
   use Ecto.Migration
 
   def change do
-    execute("ALTER TABLE blocks DROP CONSTRAINT blocks_pkey CASCADE")
-    execute("ALTER TABLE blocks ADD PRIMARY KEY (number, network)")
-
-    [:transactions, :events]
-    |> Enum.each(fn table ->
-      execute("ALTER TABLE #{table} DROP CONSTRAINT #{table}_number_fkey CASCADE")
-
-      execute("""
-        ALTER TABLE #{table}
-        ADD CONSTRAINT #{table}_block_number_network_fkey
-        FOREIGN KEY (number, network)
-        REFERENCES blocks(number, network)
-      """)
-    end)
+    execute("alter table transactions drop constraint block_number;")
+    execute("alter table blocks drop constraint blocks_pkey;")
+    execute("alter table blocks add primary key (number, network);")
+    execute("alter table transactions add constraint transactions_blocks_fk foreign key (block_number, network) references blocks (number, network);")
+    execute("alter table events add constraint events_blocks_fk foreign key (block_number, network) references blocks (number, network);")
   end
 end
