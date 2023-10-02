08c715677b449224363f3c049b6d9d8fbd16f2ef
Nacho <amigoigna@gmail.com>
2023-10-02 12:22:19 -0300

    test

--- lib/starknet_explorer_web/live/transaction_index_live.ex
+++ lib/starknet_explorer_web/live/transaction_index_live.ex
@@ -58,27 +58,27 @@ defmodule StarknetExplorerWeb.TransactionIndexLive do
               <div class="col-span-2">
                 <div class="list-h">Status</div>
                 <div>
-                  <span class={"info-label #{String.downcase(transaction.block.status)}"}>
-                    <%= transaction.block.status %>
+                  <span class={"info-label #{String.downcase(transaction.status)}"}>
+                    <%= transaction.status %>
                   </span>
                 </div>
               </div>
               <div>
                 <div class="list-h">Age</div>
-                <%= Utils.get_block_age(transaction.block) %>
+                <%= Utils.get_block_age_from_timestamp(transaction.timestamp) %>
               </div>
             </div>
           <% end %>
         </div>
       </div>
-      <CoreComponents.pagination_links id="events" page={@page} prev="dec_events" next="inc_events" />
+      <CoreComponents.pagination_links id="events" page={@page} prev="dec_txs" next="inc_txs" />
     </div>
     """
   end
 
   @impl true
   def mount(_params, _session, socket) do
-    page = Transaction.paginate_transactions(%{}, socket.assigns.network)
+    page = Transaction.paginate_transactions_for_index(%{}, socket.assigns.network)
 
     {:ok,
      assign(socket,
@@ -95,19 +95,19 @@ defmodule StarknetExplorerWeb.TransactionIndexLive do
   end
 
   @impl true
-  def handle_event("inc_events", _value, socket) do
+  def handle_event("inc_txs", _value, socket) do
     new_page_number = socket.assigns.page.page_number + 1
     pagination(socket, new_page_number)
   end
 
-  def handle_event("dec_events", _value, socket) do
+  def handle_event("dec_txs", _value, socket) do
     new_page_number = socket.assigns.page.page_number - 1
     pagination(socket, new_page_number)
   end
 
   def pagination(socket, new_page_number) do
     page =
-      Transaction.paginate_transactions(
+      Transaction.paginate_transactions_for_index(
         %{page: new_page_number},
         socket.assigns.network
       )
