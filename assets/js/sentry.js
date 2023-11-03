219122ceab3d2ef83d23b766f33db47ca6406a57
Santiago Pittella <santiagopittella@Santiagos-MacBook-Air.local>
2023-11-03 11:20:56 -0300

    use starkcompass url in prod deploy action

--- assets/js/sentry.js
+++ assets/js/sentry.js
@@ -13,7 +13,7 @@ Sentry.init({
   tracesSampleRate: 1.0,
 
   // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/(.*)madaraexplorer\.com/],
+  tracePropagationTargets: ["localhost", /^https:\/\/(.*)madaraexplorer\.com/, /^https:\/\/(.*)starkcompass\.com/],
 
   // Capture Replay for 10% of all sessions,
   // plus for 100% of sessions with an error
