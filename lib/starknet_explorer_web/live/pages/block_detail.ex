a587ecc42f0ff460c261f5ed3180c040c5e098a8
Santiago Pittella <santiagopittella@Santiagos-MacBook-Air.local>
2023-11-27 11:39:01 -0300

    fix timestamp hover in block detail

--- lib/starknet_explorer_web/live/pages/block_detail.ex
+++ lib/starknet_explorer_web/live/pages/block_detail.ex
@@ -870,20 +870,22 @@ defmodule StarknetExplorerWeb.BlockDetailLive do
             <% end %>
           </div>
         </div>
-        <div class="list-h">From Address</div>
-        <div class="block-data">
-          <div class="hash flex">
-            <a
-              href={Utils.network_path(@network, "contracts/#{event.from_address}")}
-              class="text-hover-link"
-            >
-              <%= event.from_address |> Utils.shorten_block_hash() %>
-            </a>
-            <CoreComponents.copy_button text={event.from_address} />
+        <div>
+          <div class="list-h">From Address</div>
+          <div class="block-data">
+            <div class="hash flex">
+              <a
+                href={Utils.network_path(@network, "contracts/#{event.from_address}")}
+                class="text-hover-link"
+              >
+                <%= event.from_address |> Utils.shorten_block_hash() %>
+              </a>
+              <CoreComponents.copy_button text={event.from_address} />
+            </div>
           </div>
         </div>
-        <div>
-          <div class="list-h">Age</div>
+        <div class="list-h">Age</div>
+        <div class="hash flex">
           <div class="flex items-center gap-2">
             <%= @block_age %>
             <CoreComponents.tooltip
