f7a96c35176dddac9d00a9b76faceeb29b3c8ecb
Tomás Casagrande <53660242+samoht9277@users.noreply.github.com>
2024-01-11 15:43:28 -0300

    Move starkcompass to secure VPN and change users. (#396)
    * Changed host variable to stardardize name across repos.
    
    * Changed deploy configs to change starkcompass-testing user to dev.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/home/starknet_explorer/madara_explorer/_build/prod/rel/starknet_explorer/bin/starknet_explorer start
+/home/dev/stark_compass_explorer/_build/prod/rel/starknet_explorer/bin/starknet_explorer start
